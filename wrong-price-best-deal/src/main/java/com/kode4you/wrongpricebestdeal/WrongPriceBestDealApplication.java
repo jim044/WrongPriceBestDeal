6b046976fb11f0b88de2c8b29eb386764c09b319
jimmyvillossel <jimmy.villossel@gmail.com>
2022-11-17 19:43:24 -0500

    Start project

---
+++ wrong-price-best-deal/src/main/java/com/kode4you/wrongpricebestdeal/WrongPriceBestDealApplication.java
@@ -0,0 +1,13 @@
+package com.kode4you.wrongpricebestdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrongPriceBestDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrongPriceBestDealApplication.class, args);
+	}
+
+}
---
+++ wrong-price-best-deal/src/test/java/com/kode4you/wrongpricebestdeal/WrongPriceBestDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kode4you.wrongpricebestdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrongPriceBestDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
